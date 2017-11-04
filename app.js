2e147eb5fe52cedaff07e2bcf14aa2afd1f676ad
Marcus Sundberg <marcus.sundberg87@gmail.com>
2017-11-04 15:39:43 +0100

    Ball model added to app

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const http = require('http');
 const port = 3000;
 
+var ball = {
+	x: 50.0,
+	y: 50.0,
+	xspeed: 1.0,
+	yspeed: 1.0,
+	rad: 1.0
+};
+
 const requestHandler = (request, response) => {
     console.log(request.url);
-    response.end('Hello Node.js Server!')
+    response.end(
+    	"x: " + ball.x + 
+    	", y: " + ball.y + 
+    	", xspeed: " + ball.xspeed + 
+    	", yspeed" + ball.yspeed
+    );
 };
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
+        return console.log('something bad happened', err);
     }
 
-    console.log(`server is listening on ${port}`)
+    console.log(`server is listening on ${port}`);
 });
