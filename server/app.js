30b8945bba7e8cd58a71362422d3b4812a4c4e74
martingreer <greer@student.chalmers.se>
2017-11-04 17:03:52 +0100

    Fixed path references

--- server/app.js
+++ server/app.js
@@ -1,18 +1,8 @@
-import {Ball, moveBall} from 'ball';
+import {Ball, moveBall} from './ball';
 
 const app = require('express')();
 const http = require('http').Server(app);
 const port = 3000;
-
-const requestHandler = (request, response) => {
-    console.log(request.url);
-    response.end(
-    	"x: " + Ball.x +
-    	", y: " + Ball.y +
-    	", xspeed: " + Ball.xspeed +
-    	", yspeed: " + Ball.yspeed
-    );
-};
 
 app.get('/', function(req, res){
     res.send('<h1>MultiPong server!</h1>'
